98ba1a106e59b9ac688b4a88b0d50e9cd25780d8
PhilKorte <philkorte@mail.de>
2018-11-29 18:50:54 +0100

    Änderung 29.11.18 gameOver button funktion

--- sources/javascript/mainGameOver.js
+++ sources/javascript/mainGameOver.js
@@ -1,16 +1,19 @@
 window.onload = function(){
     //init variables
-    var button1 = document.getElementById("startNextSite"); 
-    var button2 = document.getElementById("startEndSite");
+    var button1 = document.getElementById("gameOverNew"); 
+    var button2 = document.getElementById("gameOverEndSite");
+    var user = JSON.parse(localStorage.getItem("userexitStorage"));
+    var userText = document.getElementById("userText");
 
     //logic
     button1.addEventListener("click", function(event){
         console.log("button clicked");
-        window.location.href="/SoccerNoid/sources/game.html";
+        window.location.href="/SoccerNoid/sources/selectTeam.html";
     });
     button2.addEventListener("click", function(event){
         console.log("button clicked");
         window.location.href="/SoccerNoid/sources/end.html";
      });
+    userText.innerHTML = "<p>" + user.name + "<br>" + user.team + "<br>" + user.score + "</p>";
     //declare functions
 }
